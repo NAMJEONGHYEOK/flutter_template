35461ae69b5cbcc663a882e5fb07cc9a04b7588c
namjh654 <namjh654@naver.com>
2023-05-13 03:20:04 +0900

    탭바 유효성 검사 추가.

--- flutter_application_template/lib/src/widgets/finduser_widget.dart
+++ flutter_application_template/lib/src/widgets/finduser_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FindUserWidget extends StatefulWidget {
   @override
@@ -8,13 +9,18 @@ class FindUserWidget extends StatefulWidget {
 class FindUserWidgetState extends State<FindUserWidget>
     with TickerProviderStateMixin {
   // state 선언
-  final _formKey = GlobalKey<FormState>();
-  final FocusNode _focusNodeid = FocusNode();
-  final FocusNode _focusNodepassword = FocusNode();
-  final _idController = TextEditingController();
+  final _eformKey = GlobalKey<FormState>();
+  final _pformKey = GlobalKey<FormState>();
+  final FocusNode _focusNodename = FocusNode();
+  final FocusNode _focusNodeemail = FocusNode();
+  final FocusNode _focusNodephone = FocusNode();
+  final _nameController = TextEditingController();
+  final _pnameController = TextEditingController();
   final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
   AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
   late TabController _tabController;
+  int _tabbarIndex = 0;
   @override
   void initState() {
     super.initState();
@@ -22,10 +28,26 @@ class FindUserWidgetState extends State<FindUserWidget>
       length: 2,
       vsync: this, //vsync에 this 형태로 전달해야 애니메이션이 정상 처리됨
     );
+
+    // TabController의 인덱스 변경 이벤트를 감지하는 리스너 등록
+    _tabController.addListener(() {
+      if (!_tabController.indexIsChanging) {
+        _nameController.clear();
+        _emailController.clear();
+        _phoneController.clear();
+        _pnameController.clear();
+        _tabbarIndex = _tabController.index;
+        setState(() {
+          _autovalidateMode = AutovalidateMode.disabled;
+        });
+      }
+    });
   }
 
   @override
   void dispose() {
+    _focusNodename.dispose();
+    _focusNodeemail.dispose();
     _tabController.dispose();
     super.dispose();
   }
@@ -38,13 +60,19 @@ class FindUserWidgetState extends State<FindUserWidget>
           children: [
             TabBar(
               tabs: const [
-                Text(
-                  "이메일 인증",
-                  style: TextStyle(fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "이메일 인증",
+                    style: TextStyle(fontSize: 18),
+                  ),
                 ),
-                Text(
-                  "휴대폰 인증",
-                  style: TextStyle(fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text(
+                    "휴대폰 인증",
+                    style: TextStyle(fontSize: 18),
+                  ),
                 )
               ],
               controller: _tabController,
@@ -91,27 +119,30 @@ class FindUserWidgetState extends State<FindUserWidget>
     );
   }
 
+  // textformfield - onChange에서 error 확인 후 setState 로 vaildator에 전달하면 실시간 유효성 검사가능
   Widget _inputform_email(BuildContext context) {
     return Form(
-        key: _formKey,
+        key: _eformKey,
         autovalidateMode: _autovalidateMode,
         child: Column(children: [
           Container(
             margin: const EdgeInsets.all(6),
             padding: const EdgeInsets.symmetric(horizontal: 9),
             child: TextFormField(
-              focusNode: _focusNodeid,
-              validator: (id) {
-                if (id!.isEmpty) {
+              focusNode: _focusNodename,
+              validator: (name) {
+                if (name!.isEmpty) {
                   return "이름을 입력해 주세요.";
+                } else if (!RegExp(r'^[ㄱ-ㅎ|가-힣|a-z|A-Z|\s]+$').hasMatch(name)) {
+                  return '한글 또는 영어만 입력 가능합니다';
                 }
                 return null;
               },
-              keyboardType: TextInputType.text, // id text형으로 입력받기
+              keyboardType: TextInputType.text, // name text형으로 입력받기
               autocorrect: false, //자동완성 끄기.
-              autofocus: false, // 자동 초점설정 끄기
-              controller: _idController,
-              onSaved: (value) => _idController.text = value!.trim(),
+              autofocus: true, // 자동 초점설정 끄기
+              controller: _nameController,
+              onSaved: (value) => _nameController.text = value!.trim(),
               style: const TextStyle(fontSize: 15), //글자입,출력 크기조정
               decoration: InputDecoration(
                 filled: true, // 뒷 배경 색채우기
@@ -134,13 +165,17 @@ class FindUserWidgetState extends State<FindUserWidget>
               margin: const EdgeInsets.all(6),
               padding: const EdgeInsets.symmetric(horizontal: 9),
               child: TextFormField(
-                validator: (password) {
-                  if (password!.isEmpty) {
+                validator: (email) {
+                  if (email!.isEmpty) {
                     return "email를 입력해 주세요.";
+                  } else if (!RegExp(
+                          r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                      .hasMatch(email)) {
+                    return "email 형식이 아닙니다. email을 입력해 주세요";
                   }
                   return null;
                 },
-                focusNode: _focusNodepassword,
+                focusNode: _focusNodeemail,
                 autocorrect: false,
                 autofocus: false,
                 controller: _emailController,
@@ -169,25 +204,27 @@ class FindUserWidgetState extends State<FindUserWidget>
 
   Widget _inputform_phone(BuildContext context) {
     return Form(
-        key: _formKey,
+        key: _pformKey,
         autovalidateMode: _autovalidateMode,
         child: Column(children: [
           Container(
             margin: const EdgeInsets.all(6),
             padding: const EdgeInsets.symmetric(horizontal: 9),
             child: TextFormField(
-              focusNode: _focusNodeid,
-              validator: (id) {
-                if (id!.isEmpty) {
+              focusNode: _focusNodename,
+              validator: (name) {
+                if (name!.isEmpty) {
                   return "이름을 입력해 주세요.";
+                } else if (!RegExp(r'^[ㄱ-ㅎ|가-힣|a-z|A-Z|\s]+$').hasMatch(name)) {
+                  return '한글 또는 영어만 입력 가능합니다';
                 }
                 return null;
               },
               keyboardType: TextInputType.text, // id text형으로 입력받기
               autocorrect: false, //자동완성 끄기.
-              autofocus: false, // 자동 초점설정 끄기
-              controller: _idController,
-              onSaved: (value) => _idController.text = value!.trim(),
+              autofocus: true, // 자동 초점설정 끄기
+              controller: _nameController,
+              onSaved: (value) => _nameController.text = value!.trim(),
               style: const TextStyle(fontSize: 15), //글자입,출력 크기조정
               decoration: InputDecoration(
                 filled: true, // 뒷 배경 색채우기
@@ -210,19 +247,29 @@ class FindUserWidgetState extends State<FindUserWidget>
               margin: const EdgeInsets.all(6),
               padding: const EdgeInsets.symmetric(horizontal: 9),
               child: TextFormField(
-                validator: (password) {
-                  if (password!.isEmpty) {
+                validator: (phone) {
+                  if (phone!.isEmpty) {
                     return "휴대폰 번호를 입력해 주세요.";
+                  } else if (!RegExp(
+                          r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
+                      .hasMatch(phone)) {
+                    return "-없이 '01012345678' 으로 입력해주세요";
+                  } else if (phone.length != 11) {
+                    return "010을 포함한 11자리 모두 입력해주세요";
                   }
                   return null;
                 },
-                focusNode: _focusNodepassword,
+                focusNode: _focusNodephone,
                 autocorrect: false,
                 autofocus: false,
-                controller: _emailController,
-                onSaved: (value) => _emailController.text = value!.trim(),
-                keyboardType: TextInputType.text,
+                controller: _phoneController,
+                onSaved: (value) => _phoneController.text = value!.trim(),
+                keyboardType: TextInputType.number,
                 style: const TextStyle(fontSize: 15),
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                maxLength: 11,
                 decoration: InputDecoration(
                   errorStyle: TextStyle(fontSize: 12, color: Colors.red),
                   filled: true, // 뒷 배경 색채우기
@@ -251,54 +298,35 @@ class FindUserWidgetState extends State<FindUserWidget>
         child: ElevatedButton(
             // 블럭 효과보이는 버튼
             onPressed: () async {
-              _formKey.currentState!.validate();
-              _formKey.currentState!.save();
-              //id가 비었을경우
-              if (_idController.text.isEmpty) {
-                _focusNodeid.requestFocus();
+              if (_tabController.index == 0) {
+                _eformKey.currentState!.validate();
+                _eformKey.currentState!.save();
+                //이름이 비었을경우
+                if (_nameController.text.isEmpty) {
+                  _focusNodename.requestFocus();
+                }
+                //email이 비었을 경우
+                else if (_emailController.text.isEmpty) {
+                  _focusNodeemail.requestFocus();
+                }
+              } else if (_tabController.index == 1) {
+                _pformKey.currentState!.validate();
+                _pformKey.currentState!.save();
+                if (_nameController.text.isEmpty) {
+                  _focusNodename.requestFocus();
+                }
+                //phone이 비었을 경우
+                else if (_phoneController.text.isEmpty) {
+                  _focusNodephone.requestFocus();
+                } else if (!_eformKey.currentState!.validate()) {
+                  // 유효성에서 걸릴경우 state변경 후 메세지 출력
+                  setState(() {
+                    _autovalidateMode = AutovalidateMode.always;
+                  });
+                }
               }
-              //email이 비었을 경우
-              else if (_emailController.text.isEmpty) {
-                _focusNodepassword.requestFocus();
-              } else if (!_formKey.currentState!.validate()) {
-                // 유효성에서 걸릴경우 state변경 후 메세지 출력
-                setState(() {
-                  _autovalidateMode = AutovalidateMode.always;
-                });
-              }
-              //입력칸이 전부 입려된 경우. 로그인 함수로 일치, 불일치 실행.
-              else {
-                // if (await context
-                //     .read<AuthProvider>()
-                //     .login(_idController.text, _passwordController.text)) {
-                //   Navigator.pushNamed(context, 'home');
-                // } else {
-                //   _showDialog(context, "로그인 실패");
-                // }
-//               // ##
-//               final authProvider = Provider.of<AuthProvider>(context, listen: false);
-
-// // Access Token 저장
-// authProvider.accessToken = 'your_access_token_here';
-
-// // Access Token 삭제
-// authProvider.deleteAccessToken();
-
-                // Navigator.pushNamed(context, 'home');
-
-                // if (!await context
-                //     .read<AuthProvider>()
-                //     .login(_idController.text, _passwordController.text)) {
-                //   _showDialog(context, '존재하지 않는 사용자 정보입니다');
-                // } else {
-                //   context.read<Loginbutton>().Listener();
-                // }
-              }
-
-              //아직미처리..
-              // String errorMessage = '존재하지 않는 회원 정보입니다.';
-              // _showDialog(context, errorMessage);
-            }, // alert 경고창 모듈 만들어서
+              //
+            },
             style: ButtonStyle(
               shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
